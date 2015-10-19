53cfd8b5258a03c24d510ad7055a12e952aec269
Josh Long <josh@joshlong.com>
2015-10-19 12:49:38 +0300

    updated build

--- dependencies-pre.sh
+++ dependencies-pre.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-CF_USER=$0
-CF_PASSWORD=$1
-CF_ORG=$2
-CF_SPACE=$3
+CF_USER=$1
+CF_PASSWORD=$2
+CF_ORG=$3
+CF_SPACE=$4
 
 curl -v -L -o cf-cli_amd64.deb 'https://cli.run.pivotal.io/stable?release=debian64&source=github'
 sudo dpkg -i cf-cli_amd64.deb

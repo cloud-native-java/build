2031a6a89e886bd2c61baf4005349036668b23a2
Josh Long <josh@joshlong.com>
2015-11-14 19:16:37 +0000

    push

--- install.sh
+++ install.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+mkdir -p $1 && ls -la $1
+
+
+CF_USER=$1
+CF_PASSWORD=$2
+CF_ORG=$3
+CF_SPACE=$4
+
+curl -v -L -o cf-cli_amd64.deb 'https://cli.run.pivotal.io/stable?release=debian64&source=github'
+sudo dpkg -i cf-cli_amd64.deb
+
+cf api https://api.run.pivotal.io
+cf auth $CF_USER $CF_PASSWORD
+cf target -o $CF_ORG -s $CF_SPACE
+cf apps

86ec1e6cace1218153bb896aa3012d5bd1eb30c3
Josh Long <josh@joshlong.com>
2015-11-15 18:12:53 +0000

    updated

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@ export PATH=$PATH:.
 
 
 # run the tests and deploy the service
-#mvn -DskipTests=true -X clean deploy
+mvn -DskipTests=true -X clean deploy
 
 # deploy to CF
 UTILS=$( cd `dirname $0` && pwd )/utils
-$UTILS/root-cf-deploy.sh
+$UTILS/cf-root-deploy.sh

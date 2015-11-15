5bea3297620c49019d88c8ec2d97f4182b2db2e9
Josh Long <josh@joshlong.com>
2015-11-15 17:42:17 +0000

    updated

--- install.sh
+++ install.sh
@@ -9,16 +9,27 @@ CF_SPACE=$4
 
 
 
-mvn -X -DskipTests=true clean install
+#mvn -X -DskipTests=true clean install
+
+#curl -v -L -o cf-cli_amd64.deb 'https://cli.run.pivotal.io/stable?release=debian64&source=github'
+#sudo dpkg -i cf-cli_amd64.deb
+
+# https://cli.run.pivotal.io/stable?release=macosx64-binary&version=6.13.0&source=github-rel
 
 
-curl -v -L -o cf-cli_amd64.deb 'https://cli.run.pivotal.io/stable?release=debian64&source=github'
-sudo dpkg -i cf-cli_amd64.deb
 
 
 
+
+curl -v -L -o cf.tgz 'https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.13.0&source=github-rel'
+tar zxpf cf.tgz
+
+
+#
+ls -la .
+#
 cf api https://api.run.pivotal.io
 cf auth $CF_USER $CF_PASSWORD
 cf target -o $CF_ORG -s $CF_SPACE
 cf apps
-
+#

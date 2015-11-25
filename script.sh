a59a7e514914deb1b6806b8d246f255059d00cff
Josh Long <josh@joshlong.com>
2015-11-24 23:51:59 -0800

    pushing ram up for WebSphere buildpack example

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:$HOME/bin
 
 
 # run the tests and deploy the service
-mvn -DskipTests=true  clean deploy
+mvn -DskipTests=true clean deploy
 
 # deploy to CF
 UTILS=$( cd `dirname $0` && pwd )/utils

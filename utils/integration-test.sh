59025cba93e7f801d233d11b18c4a4ddea6ae05c
Josh Long <josh@joshlong.com>
2015-12-26 05:42:50 -0800

    cleanup

--- utils/integration-test.sh
+++ utils/integration-test.sh
@@ -2,19 +2,16 @@
 
 set -e
 
-source `dirname $0`/common.sh
-
-
-export ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export ROOT_DIR=`dirname $0`
 export SWAP=${SWAP:-$TMPDIR}
 
+source $ROOT_DIR/common.sh
 $ROOT_DIR/cf-common.sh
 
 function integration_test(){
     root=$1
     integration_test=$root/`basename $root`-it
-    [ -d "$integration_test" ] &&  mvn -f $integration_test/pom.xml clean install || die "there are no integration tests in '$integration_test'" 1
-
+    [ -d "$integration_test" ] &&  mvn -f $integration_test/pom.xml clean install || echo "there are no integration tests to run in '$integration_test'" 
 }
 
 integration_test `pwd`

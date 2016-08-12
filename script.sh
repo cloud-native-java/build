bb19a4b44a73bece77b4cce3f7b291325a789c5d
Josh Long <josh@joshlong.com>
2016-08-12 22:38:58 +0800

    deploy

--- script.sh
+++ script.sh
@@ -7,8 +7,8 @@ export SKIP_FILE=$PWD/skip.txt
 export BUILD_DIRECTORY=$( cd `dirname $0` && pwd )
 
 # run the tests and deploy the service
-#mvn -DskipTests=true clean deploy || die "'mvn clean deploy' failed" 1
-mvn -DskipTests=true clean install || die "'mvn clean deploy' failed" 1
+mvn -DskipTests=true clean deploy || die "'mvn clean deploy' failed" 1
+#mvn -DskipTests=true clean install || die "'mvn clean deploy' failed" 1
 # TODO uncomment the above line
 
 # remove and recreate the skip file

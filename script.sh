48c81d7e4045156f57ba4d516957f7b52a851090
Josh Long <josh@joshlong.com>
2017-02-07 16:16:57 -0800

    build

--- script.sh
+++ script.sh
@@ -7,8 +7,8 @@ export SKIP_FILE=$PWD/skip.txt
 export BUILD_DIRECTORY=$( cd `dirname $0` && pwd )
 
 # run the tests and deploy the service
-#mvn -DskipTests=true clean deploy || die "'mvn clean deploy' failed" 1
-mvn -DskipTests=true clean install || die "'mvn clean deploy' failed" 1
+mvn -DskipTests=true clean deploy || die "'mvn clean deploy' failed" 1
+#mvn -DskipTests=true clean install || die "'mvn clean deploy' failed" 1
 # TODO uncomment the above line
 
 # remove and recreate the skip file
@@ -22,7 +22,7 @@ $utils/cf-delete-orphaned-routes.sh
 $utils/root-deploy.sh
 $utils/integration-test.sh
 
-#$utils/cf-delete-orphaned-routes.sh
+$utils/cf-delete-orphaned-routes.sh
 
 cf apps
 cf services

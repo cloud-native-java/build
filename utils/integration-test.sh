f0fe7e8d418f0cd6e6a0f1f2495fe4ca4525a532
Josh Long <josh@joshlong.com>
2017-02-09 04:31:38 -0800

    build

--- utils/integration-test.sh
+++ utils/integration-test.sh
@@ -14,10 +14,8 @@ echo $it_dir
 function integration_test(){
     invoke_file_in_dir $it_dir
     mvn_pom=$it_dir/pom.xml
-    [ -f "$mvn_pom" ] && mvn -f $mvn_pom clean install  || die "'mvn clean install' failed for integration test" 1
+    [ -f "$mvn_pom" ] && mvn -X -f $mvn_pom clean install  || die "'mvn clean install' failed for integration test" 1
 }
 
 
 [ -d "$it_dir" ] && integration_test || echo "there are no integration tests to run in '$integration_test'"
-
-
